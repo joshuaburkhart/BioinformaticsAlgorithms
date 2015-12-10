3d36ebf867ac0b27cdffc18736ad449668fb4536
Joshua Burkhart <burkhart.joshua@gmail.com>
2015-12-10 03:10:56 -0800

    ..

--- final_proj/src/lib/AltDensity.py
+++ final_proj/src/lib/AltDensity.py
@@ -23,18 +23,21 @@ def max_in_window(transcript_dict, alt_set, window_size):
         print('sliding window over exon_idcs of size {0}...'.format(len(exon_idcs)))
         w_pos = 0
         while(w_pos < len(exon_idcs) - window_size + 1):
-            window = range(w_pos, w_pos + window_size)
+            window = range(exon_idcs[w_pos], exon_idcs[w_pos] + window_size)
             print('.',end="")
             sys.stdout.flush()
             print('window:{0}'.format(window))
-            first_alt_pos = 0
+            first_alt_pos = w_pos
             cur_alts = 0
-            for pos in window:
-                if pos in alt_set:
+            for exon_pos in window:
+                if exon_pos in alt_set:
                     cur_alts += 1
-                    first_alt_pos = pos + w_pos
+                first_alt_pos += 1 # just in case we need it later
             if cur_alts == 0:
-                w_pos += window_size
+                if w_pos + window_size < len(exon_idcs) - window_size + 1:
+                    w_pos += window_size
+                else:
+                    w_pos = len(exon_idcs) - window_size + 1
             elif cur_alts > max_alts:
                 print('max_alts set to {0}'.format(max_alts))
                 sys.stdout.flush()
