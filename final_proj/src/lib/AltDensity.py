5b6f305f7822fe4517eab418ddab47e926c60db6
Joshua Burkhart <burkhart.joshua@gmail.com>
2015-12-10 02:35:39 -0800

    ..

--- final_proj/src/lib/AltDensity.py
+++ final_proj/src/lib/AltDensity.py
@@ -11,14 +11,18 @@ def max_in_window(transcript_dict, alt_set, window_size):
         print('.',end="")
         sys.stdout.flush()
         exon_idcs = []
+        print('adding exons to exon_idcs...')
         for exon in transcript:
             try:
                 exon_idcs += list(range(exon[0], exon[1] + 1))
             except IndexError:
                 print('exon:{0}'.format(exon))
+        print('sorting exon_idcs in case transcript was antisense')
         exon_idcs.sort()
+        print('sliding window over exon_idcs of size {0}...'.format(len(exon_idcs)))
         for w_pos in range(0, len(exon_idcs) - window_size + 1):
             window = range(w_pos, w_pos + window_size)
+            print('window:{0}'.format(window))
             cur_alts = 0
             for pos in window:
                 if pos in alt_set:
