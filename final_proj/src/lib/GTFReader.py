e066fc0aa884f6ce9f3a33bd9b0e0b41cc75e7e2
Joshua Burkhart <burkhart.joshua@gmail.com>
2015-12-10 01:56:56 -0800

    updating .gtf Regex

--- final_proj/src/lib/GTFReader.py
+++ final_proj/src/lib/GTFReader.py
@@ -4,7 +4,7 @@ import re
 
 __author__ = 'burkhart'
 
-_exon_locus_and_transcript = '.+?\b.+?\bexon\b.*?(?P<start>[0-9]+)\b.*?(?P<end>[0-9]+)\b.*?transcript_id\s\"(?P<tid>[A-Z0-9]+)\"'
+_exon_locus_and_transcript = '.+?\t.+?\texon\t(?P<start>[0-9]+)\t(?P<end>[0-9]+)\t.+?\t.+?\t.+?\t.*?transcript_id\s\"(?P<tid>[A-Z0-9]+)\"'
 
 def parse_transcripts(gtf_filename):
     d = {}
