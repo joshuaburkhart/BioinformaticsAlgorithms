c0e6abcc9aeb9871206bbf92a8b5fa49f4a5a3cb
Joshua Burkhart <burkhart.joshua@gmail.com>
2015-12-10 02:45:27 -0800

    ..

--- final_proj/src/lib/AltDensity.py
+++ final_proj/src/lib/AltDensity.py
@@ -8,7 +8,7 @@ def max_in_window(transcript_dict, alt_set, window_size):
     max_alts = 0
     #TODO: Use a threadpool to execute this loop
     for transcript in transcript_dict.values():
-        print('.',end="")
+        print('T',end="")
         sys.stdout.flush()
         exon_idcs = []
         print('adding exons to exon_idcs...')
@@ -20,15 +20,23 @@ def max_in_window(transcript_dict, alt_set, window_size):
         print('sorting exon_idcs in case transcript was antisense')
         exon_idcs.sort()
         print('sliding window over exon_idcs of size {0}...'.format(len(exon_idcs)))
-        for w_pos in range(0, len(exon_idcs) - window_size + 1):
+        w_pos = 0
+        while(w_pos < len(exon_idcs) - window_size + 1):
             window = range(w_pos, w_pos + window_size)
+            print('.',end="")
+            sys.stdout.flush()
             print('window:{0}'.format(window))
+            first_alt_pos = 0
             cur_alts = 0
             for pos in window:
                 if pos in alt_set:
                     cur_alts += 1
-            if cur_alts > max_alts:
+                    first_alt_pos = pos + w_pos
+            if cur_alts == 0:
+                w_pos = cur_alts
+            elif cur_alts > max_alts:
                 print('max_alts set to {0}'.format(max_alts))
                 sys.stdout.flush()
                 max_alts = cur_alts
+                w_pos = first_alt_pos
     return max_alts
