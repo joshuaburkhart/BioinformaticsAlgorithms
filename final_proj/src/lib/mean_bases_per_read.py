a58c681ad1fe7efe62ab15f9e113c4b0d04348f3
Joshua Burkhart <burkhart.joshua@gmail.com>
2015-12-12 02:34:48 -0800

    mean_read_bases counting looks right

--- final_proj/src/lib/mean_bases_per_read.py
+++ final_proj/src/lib/mean_bases_per_read.py
@@ -8,13 +8,15 @@ _author__ = 'burkhart'
 def parse_fastq(fq):
     d = {}
     with open(fq) as FileObj:
+        next(FileObj)
         for line in FileObj:
-            next(FileObj)
             Ns = line.count('N')
             if Ns in d:
                 d[Ns] += 1
             else:
                 d[Ns] = 1
+                print('Read with {0} Ns detected: {1}'.format(Ns,line.rstrip()))
+            next(FileObj)
             next(FileObj)
             next(FileObj)
     print(d)
