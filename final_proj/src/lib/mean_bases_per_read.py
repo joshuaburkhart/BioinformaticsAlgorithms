d5b4b4773dba858d7b8545284fbb8826b9213ef7
Joshua Burkhart <burkhart.joshua@gmail.com>
2015-12-12 02:23:21 -0800

    trying another file iterator

--- final_proj/src/lib/mean_bases_per_read.py
+++ final_proj/src/lib/mean_bases_per_read.py
@@ -1,19 +1,22 @@
 
 import sys
 
-__author__ = 'burkhart'
-
 #Usage: python3 mean_bases_per_read.py ~/input/reads.fasta
+
+_author__ = 'burkhart'
 
 def parse_fastq(fq):
     d = {}
     with open(fq) as FileObj:
-        for line in FileObj[2:]:
+        for line in FileObj:
+            next(FileObj)
             Ns = line.count('N')
             if Ns in d:
                 d[Ns] += 1
             else:
                 d[Ns] = 1
+            next(FileObj)
+            next(FileObj)
     print(d)
 
 parse_fastq(sys.argv[1])
