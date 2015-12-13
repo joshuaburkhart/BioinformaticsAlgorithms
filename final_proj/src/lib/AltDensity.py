d9122592aba551fe60bda1b301637e52d5b4b1e0
Joshua Burkhart <burkhart.joshua@gmail.com>
2015-12-12 17:53:26 -0800

    debugging

--- final_proj/src/lib/AltDensity.py
+++ final_proj/src/lib/AltDensity.py
@@ -58,7 +58,7 @@ def max_in_sorted_t(exon_idcs,alt_set,window_size,tid):
     #print('sliding window over exon_idcs of size {0}...'.format(len(exon_idcs)))
     w_pos = 0
     while(w_pos < len(exon_idcs) - window_size + 1):
-        window = range(exon_idcs[w_pos], exon_idcs[w_pos + window_size])
+        window = exon_idcs[w_pos:w_pos + window_size]
         #print('.',end="")
         #sys.stdout.flush()
         #print('window:{0}'.format(window))
